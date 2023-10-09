6123b7f0dad466a1a4080aebd42737193c300c8f
TexHaotianTan <146574402+TexHaotianTan@users.noreply.github.com>
2023-10-09 11:53:20 +0000

    bootcamp session 1

---
+++ week2/rectangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    float width, length, area;
+
+    printf("What is the width? ");
+    scanf("%f" , &width);
+    printf("\nWhat is the length? ");
+    scanf("%f" , &length);
+
+    area = width * length;
+
+    printf("The area is: %.3g\n", area);
+
+    return 0;
+
+}
